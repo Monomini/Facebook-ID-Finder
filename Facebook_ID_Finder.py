32d48055e16e96aa3fa583a032a4ad7d2d9300d0
monomini <monomini@gmail.com>
2019-05-30 08:58:55 +0800

    add ID2NAME

--- Facebook_ID_Finder.py
+++ Facebook_ID_Finder.py
@@ -1,19 +1,84 @@
-import requests, re, argparse, sys
+import re, argparse, sys, json, datetime, urllib.parse
+from  requests_html import HTMLSession
 
 parser = argparse.ArgumentParser()
-parser.add_argument("target", help="Set the target.")
 
+parser.add_argument("-u", "--uid" , help="Input user's UID.", dest="uid")
+parser.add_argument("-f", "--file", help="File contain UIDs in Json format.", metavar="File Path", dest="user_file")
+parser.add_argument("-t", "--target", help= "Get UID from target URL.", metavar="URL", dest="target")
 args = parser.parse_args()
 
-target = args.target
-result = requests.get(target)
+time = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
 
-if result.ok:
-    result = result.text
-    profile_id = re.search(r"profile_id=[0-9]+", result)
-    profile_id = re.search(r"[0-9]+", profile_id.group()).group() if profile_id else 'permission denied'
-    print(profile_id)
+def Id2NameError(uid):
+	session = HTMLSession()
+	url = "https://www.facebook.com/profile.php?id={}".format(uid)
+	result = session.get(url)
+	result.html.render()
+
+	timeline = result.html.find("span#fb-timeline-cover-name", first=True)
+	if timeline:
+		username = timeline.text
+		userpage = list(timeline.links)[0]
+		print("username = {}\nURL      = {}".format(username, userpage))
+	else:
+		timeline = result.html.find("#seo_h1_tag", first=True)
+		if timeline:
+			username = timeline.text
+			userpage = list(timeline.links)[0]
+			print("username = {}\nURL      = {}".format(username, userpage))
+		else:
+			username = uid
+			print("Error\nusername = {}\nURL      = {}".format(username, userpage))
+
+	if args.user_file:
+		with open("{}_.txt".format(time), 'a+') as f:  
+			f.write("\nusername = {}\nURL      = {}".format(username, userpage))
+def Id2Name(uid):
+	session = HTMLSession()
+	url = "https://www.facebook.com/profile.php?id={}".format(uid)
+	result = session.get(url)
+	redirect = result.html.search("window.location.replace(\"{}\")")[0]
+	userpage = redirect.replace('\\','')
+	result = session.get(userpage)
+	print(userpage)
+	
+	timeline = result.html.find("span#fb-timeline-cover-name", first=True)
+	if timeline:
+		username = timeline.text
+		print("username = {}\nURL      = {}".format(username, userpage))
+	else:
+		timeline = result.html.find("#pageTitle", first=True)
+		if timeline:
+			username = timeline.text.split(' - 首頁 | Facebook')[0]
+			print("username = {}\nURL      = {}".format(username, userpage))
+		else:
+			Id2NameError(uid)
+			return	
+	if args.user_file:
+		with open("{}_.txt".format(time), 'a+') as f:  
+			f.write("\nusername = {}\nURL      = {}".format(username, userpage))
+
+if args.target:
+	session = HTMLSession()
+	result = session.get(" https://www.facebook.com/jeff.hein.71")
+	if result:
+		profile_id = result.html.search("\"fb://profile/{}\"")[0]
+		if not profile_id:
+			profile_id = result.html.search("page_id={}\"")[0]
+		print("uid = {}".format(profile_id))
+	else:
+	    	print('The target is invalid page.')
+
+elif args.uid:
+	Id2Name(args.uid)	
+elif args.user_file:
+	with open(args.user_file) as json_file:  
+		data = json.load(json_file)
+	for ids in data:
+		Id2Name(ids)
+	
 else:
-    print('The target is invalid page.')
+	parser.print_help()
 
 
